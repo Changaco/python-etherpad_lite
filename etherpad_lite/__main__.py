12c9846eda091bed704633cdbe1e54b07c76506f
Changaco <changaco@changaco.net>
2012-09-21 17:18:25 +0200

    add README, license and packaging stuff

--- etherpad_lite/__main__.py
+++ etherpad_lite/__main__.py
@@ -1,3 +1,11 @@
+# This file is part of a program licensed under the terms of the GNU Lesser
+# General Public License version 3 (or at your option any later version)
+# as published by the Free Software Foundation.
+#
+# If you have not received a copy of the GNU Lesser General Public License
+# along with this file, see <http://www.gnu.org/licenses/>.
+
+
 from argparse import Action, ArgumentParser, SUPPRESS
 import os
 import shlex
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+from version import get_version
+
+setup(
+    name='etherpad_lite',
+    version=get_version(),
+    description='Python interface for Etherpad-Lite\'s HTTP API',
+    author='Changaco',
+    author_email='changaco ατ changaco δοτ net',
+    url='http://changaco.net/gitweb/?p=python-etherpad_lite.git',
+    license='LGPLv3+',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README')).read(),
+)
