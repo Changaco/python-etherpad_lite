19a3bb941c31000982a4fd05e25e8f6fe09e142d
Changaco <changaco@changaco.oy.lc>
2016-06-01 12:07:21 +0200

    encode POST data to utf8 ourselves, closes #6

--- etherpad_lite/__init__.py
+++ etherpad_lite/__init__.py
@@ -8,6 +8,14 @@ except ImportError:
     from urllib import urlencode
 
 
+def utf8_encode(s):
+    return s if isinstance(s, bytes) else s.encode('utf8')
+
+
+def utf8_encode_dict_values(d):
+    return {k: utf8_encode(v) for k, v in d.items()}
+
+
 class EtherpadException(Exception): pass
 
 
@@ -16,11 +24,12 @@ class EtherpadLiteClient(object):
     def __init__(self, base_params={}, base_url='http://localhost:9001/api',
                        api_version='1', timeout=20):
         self.api_version = api_version
-        self.base_params = base_params
+        self.base_params = utf8_encode_dict_values(base_params)
         self.base_url = base_url
         self.timeout = timeout
 
     def __call__(self, path, **params):
+        params = utf8_encode_dict_values(params)
         data = urlencode(dict(self.base_params, **params)).encode('ascii')
         url = '%s/%s/%s' % (self.base_url, self.api_version, path)
         r = json.loads(urlopen(url, data, self.timeout).read().decode('utf-8'))
